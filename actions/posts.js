5f81d0e66cb5f88035a6c445dce5ae242dd5060e
faysal725 <afaysal725@gmail.com>
2025-03-03 10:48:00 +0600

    like button modified

--- actions/posts.js
+++ actions/posts.js
@@ -1,7 +1,7 @@
 "use server";
 
 import { redirect } from "next/navigation";
-import { storePost } from "@/lib/posts";
+import { storePost, updatePostLikeStatus } from "@/lib/posts";
 import { uploadImage } from "@/lib/cloudnary";
 
 export async function createPost(prevState, formData) {
@@ -38,4 +38,8 @@ export async function createPost(prevState, formData) {
   await storePost({ title, imageUrl: imageUrl, content, userId: 1 });
 
   redirect("/feed");
+}
+
+export async function togglePostLikeStatus(postId) {
+  updatePostLikeStatus(postId, 2);
 }
